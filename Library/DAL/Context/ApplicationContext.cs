962916faa3e9586287b43d1622cfc67c81a3c09f
NakkiCalvin <31292872+NakkiCalvin@users.noreply.github.com>
2019-03-04 21:18:45 +0300

    FirstComit

---
+++ Library/DAL/Context/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
